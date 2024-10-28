3847e8e1057559a50b3510342196de8ae9d1fd1f
LucasMonfardini <131724808+LucasMonfardini@users.noreply.github.com>
2024-10-28 20:06:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -22,14 +22,14 @@ recognition.onresult = function(event)
   var content = event.results[0][0].transcript;
 
   document.getElementById("status").innerHTML = "A fala foi reconhecida como: " + content;
-  if(content = "circulo")
+  if(content == "círculo")
   {
    x = Math.floor(Math.random() * 900);
    y = Math.floor(Math.random() * 600);
    document.getElementById("status").innerHTML ="Desenhado um círculo.";
    drawCircle = "set"
   }
-  if(content = "retângulo")
+  if(content == "retângulo")
     {
      x = Math.floor(Math.random() * 900);
      y = Math.floor(Math.random() * 600);
